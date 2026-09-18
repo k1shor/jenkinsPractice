e8718e4b9ddafa9c733264e21ae2b2cbe23b7984
Kishor Munikar <k1shor@hotmail.com>
2026-09-18 15:28:27 +0545

    minor changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send("Hello world !!!"))
-app.get('/hi', (req, res) => res.send("Good Afternoon !!!"))
+app.get('/hi', (req, res) => res.send("Good Afternoon !!"))
 
 app.listen(5000, ()=>{
     console.log("Server is running on port 5000")
